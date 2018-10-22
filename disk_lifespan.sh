ae2579f004d49b9b650af53a6276fe7b9b3f0ef4
Simon Claret <sclaret@gmail.com>
2018-10-21 20:36:46 -0400

    Add plan for lifespan testing

---
+++ disk_lifespan.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+# Set TIME_START
+# Write 256 megabytes * 4
+# Set TIME_FINISH
+# Update GIGABYTES_WRITTEN, TIME_TAKEN
+# echo {timestamp, GIGABYTES_WRITTEN, TIME_TAKEN} | sqlite
+# Run with nohup
+
+# Node API for access to sqlite
+# localtunnel to http server
+
+# React monitor / alert / query tool
+# localtunnel down? "Error: unable to reach API"
+# graphing
+# view past tests
+# generalize for other benchmarks
+#
+# user contributions
+# check prior art (userbenchmarks)
