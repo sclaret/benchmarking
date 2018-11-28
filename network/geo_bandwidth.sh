adc749d08587d74528fa530e66124a792bc21441
Simon Claret <sclaret@gmail.com>
2018-11-28 13:13:27 -0500

    Add network test

---
+++ network/geo_bandwidth.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+# Montreal
+wget http://205.204.85.213/speedtest.256mb
+
+# Toronto
+wget http://172.98.73.4/speedtest.256mb
+
+# Miami
+wget http://172.98.77.20/speedtest.256mb
+
+# London
+wget http://82.163.78.28/speedtest.256mb
+
+# Paris
+wget http://176.67.168.246/speedtest.256mb
+
+# Sydney
+wget http://119.252.188.245/speedtest.256mb
+
+
